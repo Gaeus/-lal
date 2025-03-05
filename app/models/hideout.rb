538259b1b4b35f75b4e15b6e8b5060e4d71661e8
Paul Fouquet <Skypol59@msn.com>
2025-03-05 11:55:16 +0100

    version fonctionnelle de la database a debugguer

--- app/models/hideout.rb
+++ app/models/hideout.rb
@@ -3,9 +3,9 @@ class Hideout < ApplicationRecord
   belongs_to :user
 
   validates :user, presence: true
-  validates :address, presence: true, length: { in: 1..50 }, uniqueness: true
+  validates :address, presence: true, length: { in: 1..150 }, uniqueness: true
   validates :price, presence: true, numericality: true
   validates :overview, presence: true, length: { maximum: 500 }
-  validates :title, presence: true, length: { in: 1..50 }, uniqueness: true
+  validates :title, presence: true, length: { in: 1..150 } # , uniqueness: true /Gaeus : disabled for testing
   validates :cover_picture, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :hideout
   has_many :booking
 
-  validates :nickname, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :nickname, presence: true, length: { maximum: 30 }
   validates :first_name, presence: true, length: { maximum: 30 }
   validates :last_name, presence:true, length: { maximum: 30 }
 
